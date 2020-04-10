98339e4e205b4f9c4239d899ea247f8f8396f330
prabhaat <anshum.52@gmail.com>
2020-04-11 02:39:39 +0530

    piegonHole Working of 2 not for 17

---
+++ Piegonhole.cpp
@@ -0,0 +1,55 @@
+/*  
+  The Problem 
+  Given a number n , find the number consisting of 0 and 1 only that is divisible by n
+*/
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{   int r;
+    int n = 17;
+    int number[n+1] = {0};
+    int remainder[n+1] = {0};
+    int d = 1;
+    int answer;
+    int i,j;
+
+    for( i = 1;i<=n;i++)
+    {
+         if(d%n == 0)
+         {
+             cout<<d<<endl;
+             break;
+         }
+         
+         if(remainder[d%n] == 0)
+         {
+             remainder[d%n] = i;
+             cout<<"in if loop value of i = "<<i<<endl;
+             cout<<"in if loop value of d = "<<d<<endl;
+             cout<<"in if loop value of d%n = "<<d%n<<endl;
+             cout<<"in if loop value of r = "<<remainder[d%n]<<endl;
+
+         }
+         else
+         {
+              for(j = 0;j<i-remainder[d%n];j++)
+              {
+                  cout<<1;
+              }
+              for(j = 0;j<remainder[d%n];j++)
+              {
+                  cout<<0;
+              }
+              break;
+         }
+
+         d = (d*10) + 1;
+    }
+
+
+   // cout<<"answer = "<<answer;
+    return 0;
+}
