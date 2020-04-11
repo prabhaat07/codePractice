a9d4baab4d87d65e9f85bb4955f24dea4d0ca981
prabhaat <anshum.52@gmail.com>
2020-04-11 19:29:25 +0530

    Range update question

---
+++ RangeUpdate.cpp
@@ -0,0 +1,57 @@
+/*
+The problem : 
+Suppose we have array a[] 
+for each  query q(x,y,val)
+update a such that 
+
+from xth element to yth element 
+Add the value val to each element within these indices
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int sum[100] = {0};
+
+void PartialSum(int arr[], int length)
+{
+    for (int i = 0; i < length; i++)
+    {
+        if (i > 0)
+        {
+            sum[i] = sum[i - 1] + arr[i];
+        }
+        else
+        {
+            sum[i] = arr[i];
+        }
+    }
+}
+
+void Query(int x, int y, int val)
+{
+
+    sum[x] = sum[x] + val;
+    sum[y + 1] = sum[y + 1] - val;
+}
+
+int main()
+{
+    int length;
+
+    int arr[length];
+
+    int x, y, val;
+
+    Query(x, y, val);
+
+    PartialSum(arr, length);
+
+    for (int i = 0; i < length; i++)
+    {
+        cout << sum[i] << " ";
+    }
+
+    return 0;
+}
