4fce43f0a3f5e8c521f06616bb2b016b334352b9
prabhaat <anshum.52@gmail.com>
2020-04-15 01:08:17 +0530

    Maximum SumSubArray Greedy Aproach

---
+++ MaximumSumSubArray.cpp
@@ -0,0 +1,33 @@
+//Greedy Aproach
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int MaxSum(int arr[], int length)
+{
+    int ans = arr[0], sum;
+
+    for (int i = 0; i < length; i++)
+    {
+        sum += arr[i];
+        if (sum > ans)
+        {
+            ans = sum;
+        }
+        if (sum < 0)
+        {
+            sum = 0;
+        }
+    }
+
+    return ans;
+}
+
+int main()
+{
+    int arr[5] = {0};
+
+    int answer = MaxSum(arr, 5);
+    return 0;
+}
